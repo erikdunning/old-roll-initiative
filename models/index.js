54db301f241a9ae54af1f510bde7b404c1ee48a3
Erik Dunning <erikdunning@gmail.com>
2017-09-04 10:09:21 -0500

    More cleanup.

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const models = ['User'];
 
 module.exports = sequelize => {
   const mapped = {};
   models.forEach(model => {
-    mapped[model] = sequelize.import(__dirname + '/' + model.toLowerCase());
+    mapped[model] = sequelize.import(path.join(__dirname, model.toLowerCase()));
   });
   return mapped;
 }
